fb921369ff89f792827ad569ead3376bdaa126ed
thumbsu <uumj222@gmail.com>
2023-01-01 17:43:35 +0900

    :tada: BEGIN: prettier config project

---
+++ index.js
@@ -0,0 +1,11 @@
+module.exports = {
+  bracketSameLine: true,
+  singleQuote: true,
+  trailingComma: "all",
+  bracketSpacing: true,
+  semi: false,
+  tabWidth: 2,
+  printWidth: 100,
+  endOfLine: "lf",
+  arrowParens: "avoid",
+};
